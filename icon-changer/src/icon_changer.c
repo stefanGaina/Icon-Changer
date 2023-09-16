fd3e108e50fc0ce62a1856a743ef6d33c6096f9f
stefanGaina <stefan.gaina01@gmail.com>
2023-09-16 12:58:09 +0300

    Minor changes

--- icon-changer/src/icon_changer.c
+++ icon-changer/src/icon_changer.c
@@ -2,6 +2,7 @@
  * @file icon_changer.c                                                                               *
  * @date:      @author:                   Reason for change:                                          *
  * 14.08.2023  Gaina Stefan               Initial version.                                            *
+ * 16.09.2023  Gaina Stefan               Added checks for files existence.                           *
  * @details This file implements the interface defined in icon_changer.h.                             *
  * @todo N/A.                                                                                         *
  * @bug No known bugs.                                                                                *
@@ -18,23 +19,44 @@
 #include "icon_changer.h"
 
 /******************************************************************************************************
+ * FUNCTION PROTOTYPES                                                                                *
+ *****************************************************************************************************/
+
+/**
+ * @brief https://docs.microsoft.com/en-us/cpp/c-runtime-library/reference/access-waccess?view=msvc-160
+*/
+extern int access(const char* path, int mode);
+
+/******************************************************************************************************
  * FUNCTION DEFINITIONS                                                                               *
  *****************************************************************************************************/
 
 bool change_executable_icon(const char* const icon_path, const char* const executable_path)
 {
-	void* const icon_file         = CreateFileA(icon_path, 0x80000000L, 0L, NULL, 3L, 0L, NULL);
-	void*       icon_info         = NULL;
-	int32_t     image_offset      = 0L;
-	int32_t     image_size        = 0L;
-	void*       icon_read_buffer  = NULL;
-	void*       update_resource   = NULL;
+	void* const icon_file        = CreateFileA(icon_path, 0x80000000L, 0L, NULL, 3L, 0L, NULL);
+	void*       icon_info        = NULL;
+	int32_t     image_offset     = 0L;
+	int32_t     image_size       = 0L;
+	void*       icon_read_buffer = NULL;
+	void*       update_resource  = NULL;
+
+	if (-1L == access(icon_path, 0L))
+	{
+		(void)fprintf(stdout, "Icon not found!\n");
+		return false;
+	}
+
+	if (-1L == access(executable_path, 0L))
+	{
+		(void)fprintf(stdout, "Executable not found!\n");
+		return false;
+	}
 
 	/* read 1st 22 bytes from ico file: https://en.wikipedia.org/wiki/ICO_(file_format) */
 	icon_info = malloc(22ULL);
 	if (NULL == icon_info)
 	{
-		(void)fprintf(stdout, "Out of memory! Couldn't allocate %" PRIu64 "bytes\n", 22ULL);
+		(void)fprintf(stdout, "Couldn't allocate memory for icon information! (bytes: %" PRIu64 ")\n", 22ULL);
 		return false;
 	}
 
@@ -56,7 +78,7 @@ bool change_executable_icon(const char* const icon_path, const char* const execu
 	icon_read_buffer = malloc((size_t)image_size);
 	if (NULL == icon_read_buffer)
 	{
-		(void)fprintf(stdout, "Out of memory! Couldn't allocate %" PRId32 "bytes\n", image_size);
+		(void)fprintf(stdout, "Couldn't allocate memory for icon buffer! (bytes: %" PRId32 ")\n", image_size);
 		free(icon_info);
 		(void)CloseHandle(icon_file);
 
--- icon-changer/src/icon_changer_main.c
+++ icon-changer/src/icon_changer_main.c
@@ -1,9 +1,10 @@
 /******************************************************************************************************
- * @file main.c                                                                                       *
+ * @file icon_changer_main.c                                                                          *
  * @date:      @author:                   Reason for change:                                          *
  * 27.06.2023  Gaina Stefan               Initial version.                                            *
  * 05.08.2023  Gaina Stefan               Made parameterized pointers const.                          *
  * 14.08.2023  Gaina Stefan               Moved the change icon function.                             *
+ * 16.09.2023  Gaina Stefan               Renamed file name.                                          *
  * @details This file provides an entry point for the tool and also the algorithm that does the       *
  * change of the icon.                                                                                *
  * @todo N/A.                                                                                         *
@@ -28,26 +29,17 @@
 /**
  * @brief The major version of the tool.
 */
-#define VERSION_MAJOR (uint8_t)1U
+static const uint8_t VERSION_MAJOR = 1U;
 
 /**
  * @brief The minor version of the tool.
 */
-#define VERSION_MINOR (uint8_t)0U
+static const uint8_t VERSION_MINOR = 0U;
 
 /**
  * @brief The patch version of the tool.
 */
-#define VERSION_PATCH (uint8_t)2U
-
-/******************************************************************************************************
- * FUNCTION PROTOTYPES                                                                                *
- *****************************************************************************************************/
-
-/**
- * @brief https://docs.microsoft.com/en-us/cpp/c-runtime-library/reference/access-waccess?view=msvc-160
-*/
-extern int access(const char* path, int mode);
+static const uint8_t VERSION_PATCH = 0U;
 
 /******************************************************************************************************
  * ENTRY POINT                                                                                        *
@@ -55,7 +47,7 @@ extern int access(const char* path, int mode);
 
 int main(int argc, char* argv[])
 {
-	if (2L == argc && 0L == strcmp("--version", argv[1]))
+	if (2L == argc && (0L == strcmp("--version", argv[1]) || 0L == strcmp("-v", argv[1])))
 	{
 		(void)fprintf(stdout, "Icon-Changer version %" PRIu8 ".%" PRIu8 " .%" PRIu8 "!\n", VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH);
 		return EXIT_SUCCESS;
@@ -63,19 +55,7 @@ int main(int argc, char* argv[])
 
 	if (3L > argc)
 	{
-		(void)fprintf(stdout, "Usage: <%s.exe> <path_to_icon> <path_to_exe>\n", argv[0]);
-		return EXIT_FAILURE;
-	}
-
-	if (-1L == access(argv[1], 0L))
-	{
-		(void)fprintf(stdout, "Icon not found!\n");
-		return EXIT_FAILURE;
-	}
-
-	if (-1L == access(argv[2], 0L))
-	{
-		(void)fprintf(stdout, "Executable not found!\n");
+		(void)fprintf(stdout, "Usage: <%s> <path_to_icon> <path_to_exe>\n", argv[0]);
 		return EXIT_FAILURE;
 	}
 
